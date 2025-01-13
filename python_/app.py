28a0192e9002a6717d8eb1f6cdc5e2c6eb07fd86
Dmitry Savkov <dmitry_savkov@mail.ru>
2025-01-13 22:12:43 +0500

    Added lib directory path in system PATH to resolve issue with not found EGL

--- python_/app.py
+++ python_/app.py
@@ -7,6 +7,10 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 
+mesa_dll_path = r"C:/tools/mesa/x64"  # Update this path as needed
+os.environ['PATH'] = mesa_dll_path + ";" + os.environ.get('PATH', '')
+print(os.environ['PATH'])
+
 import cmake_example as game
 
 def main():
@@ -17,13 +21,14 @@ def main():
         print(f"Failed to create AppDelegate instance: {e}")
         return
 
+    print(app_delegate.__dir__())
+    app_delegate.applicationDidFinishLaunching()
     # Initialize the game with the AppDelegate instance
-    success = app_delegate.applicationDidFinishLaunching()
     '''try:
         success = game.init(app_delegate)
     except Exception as e:
         print(f"Initialization encountered an error: {e}")
-        return'''
+        return
     
     if success:
         print("Initialization successful.")
@@ -34,7 +39,7 @@ def main():
         except KeyboardInterrupt:
             print("Game terminated by user.")
     else:
-        print("Initialization failed.")
+        print("Initialization failed.")'''
 
 if __name__ == "__main__":
     main()
