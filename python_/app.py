99a391795d0b8111c834d08d737341f83a069306
Dmitry Savkov <dmitry_savkov@mail.ru>
2025-01-14 09:02:30 +0500

    Used anlge dlls and environ path to the pyd as a dlls resource. Working.

--- python_/app.py
+++ python_/app.py
@@ -2,13 +2,16 @@ import os
 import sys
 import inspect
 import time
+import traceback
+import pathlib
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir) 
 
+path = os.path.join(pathlib.Path(__file__).parent.parent, "cmake_example/build/lib.win-amd64-cpython-312")
 mesa_dll_path = r"C:/tools/mesa/x64"  # Update this path as needed
-os.environ['PATH'] = mesa_dll_path + ";" + os.environ.get('PATH', '')
+os.environ['PATH'] = path + ";" + os.environ.get('PATH', '')
 print(os.environ['PATH'])
 
 import cmake_example as game
@@ -19,15 +22,17 @@ def main():
         app_delegate = game.AppDelegate()
     except Exception as e:
         print(f"Failed to create AppDelegate instance: {e}")
+        traceback.print_exc()
         return
 
-    print(app_delegate.__dir__())
-    app_delegate.applicationDidFinishLaunching()
     # Initialize the game with the AppDelegate instance
-    '''try:
+    try:
+        print('something')
         success = game.init(app_delegate)
+        print('something')
     except Exception as e:
         print(f"Initialization encountered an error: {e}")
+        traceback.print_exc()
         return
     
     if success:
@@ -39,7 +44,7 @@ def main():
         except KeyboardInterrupt:
             print("Game terminated by user.")
     else:
-        print("Initialization failed.")'''
+        print("Initialization failed.")
 
 if __name__ == "__main__":
     main()
